a1bd5b8c1c47fe1ff4cf9262acd18e31c5aacd2c
Ash709 <ashlinmishra2003@gmail.com>
2025-10-29 20:44:11 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,10 +1,8 @@
 import cv2
 from ultralytics import YOLO
 
-# Load YOLO model (pretrained on COCO dataset)
 model = YOLO("yolov8n.pt")
 
-# Open video stream (0 for webcam or path to video file)
 cap = cv2.VideoCapture("Video/lane2.mp4")
 
 while cap.isOpened():
@@ -12,13 +10,13 @@ while cap.isOpened():
     if not ret:
         break
     frame = cv2.resize(frame, (400, 400))
-    # Perform object detection
+   
     results = model(frame)
 
-    # Draw bounding boxes
+    
     for result in results:
         for box in result.boxes:
-            x1, y1, x2, y2 = map(int, box.xyxy[0])  # Get coordinates
+            x1, y1, x2, y2 = map(int, box.xyxy[0])  
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
     cv2.imshow("Traffic Detection", frame)
